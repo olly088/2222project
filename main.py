81088d2394a627dae38ab730071a79d3a73b60d7
Oliver Etherington <oeth7978@uni.sydney.edu.au>
2020-09-20 23:42:54 +1000

    Project Start

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+	#Your main function here
+	pass
+
+if __name__ == "__main__":
+	main()
---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class /Test(unittesst.TestCase):
+	def test_hello(self):
+		hello = "Hello World"
+self.assertEquals(hello, "Hello World")
+
+
+if __name__ == "__main__":
+	unittest.main()
